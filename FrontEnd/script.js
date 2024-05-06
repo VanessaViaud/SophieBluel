00e308773bb85de44f2dd301b2339d4d2fc50216
VanessaViaud <vanessa.viaud@gmail.com>
2024-05-06 15:51:36 +0200

    feat/complete_modal

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,8 @@
-    //variables globales
+//variables globales
 let works = [];
 const galleryElement = document.querySelector(".gallery");
 
-    //appel API categories et works
+//appel API categories et works
 async function getWorks() {
     const categoriesResponse = await fetch("http://localhost:5678/api/categories");
     const categories = await categoriesResponse.json();
@@ -12,11 +12,13 @@ async function getWorks() {
 
     createbutton(categories);
     generateGallery(works);
+    generateGalleryModal(works);
     renderEditionMode();
-    
 }
-    //créer fonction qui génère les works (img + title)
+
+//créer fonction qui génère les works (img + title)
 function generateGallery(works) {
+    galleryElement.innerHTML = "";
 
     for (let i = 0; i < works.length; i++) {
         const figure = works[i];
@@ -31,6 +33,49 @@ function generateGallery(works) {
         figureElement.appendChild(nomElement);
     }
 }
+
+//créer fonction qui génère les works dans la modal
+function generateGalleryModal(works) {
+    const galleryModalElement = document.querySelector(".gallery-modal");
+    galleryModalElement.innerHTML = "";
+
+    for (let i = 0; i < works.length; i++) {
+        const figure = works[i];
+        const figureElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.src = figure.imageUrl;
+
+        const trashElement = document.createElement("button");
+        trashElement.className = "modal-trash-can";
+        trashElement.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+        trashElement.addEventListener("click", function (e) {
+            e.preventDefault();
+            deleteWork(works[i].id, e);
+        });
+
+        galleryModalElement.appendChild(figureElement);
+        figureElement.appendChild(trashElement);
+        figureElement.appendChild(imageElement);
+    }
+}
+
+// fonction pour supprimer un work à partir de l'id
+async function deleteWork(id, e) {
+    e.preventDefault();
+    const authToken = localStorage.getItem("authToken")
+    const deleteResponse = await fetch("http://localhost:5678/api/works/"+id, 
+    {
+        method: "DELETE",
+        headers: {
+            Authorization: "Bearer " + authToken
+        }
+    });
+    generateGalleryModal();
+    deleted = await deleteResponse.json();
+    await getWorks();
+
+}
+
 //créer fonction filtre selon categorie
 // definition categorie : Array contenant toutes les categoryId.
 // Exemple: [1,2,3]
@@ -52,7 +97,7 @@ function filterGallery(categorie) {
     // 3: rappeler generateGallery avec notre nouveau tableau de works
     generateGallery(filteredWorks);
 }
-    //générer mes boutons à partir des categories
+//générer mes boutons à partir des categories
 function createbutton(categories) {
     const filtersElement = document.querySelector(".filters");
 
@@ -90,6 +135,7 @@ export function editorView() {
     window.location.href = "index.html";    
 }
 
+//créer fonction pour générer le Mode Edition
 function renderEditionMode() {
     if(localStorage.getItem("authToken") !== null) {
         const subHeaderSection = document.createElement("section");
@@ -107,35 +153,39 @@ function renderEditionMode() {
         modifierEditorButton.addEventListener("click", openModal);
         modifierEditorDiv.appendChild(modifierEditorButton);
         
-        
         const SdParentElement = document.querySelector(".titleProjets");
         SdParentElement.appendChild(modifierEditorDiv);        
-
+        
+        const filtersElement = document.querySelector(".filters");
+        filtersElement.style.display = 'none';
         };
-     if(localStorage.getItem("authToken") !== null) {
-        const logLink = document.getElementById("#loginLink");
-        logLink.innerHTML = "Logout";
+        
+    if(localStorage.getItem("authToken") !== null) {
+        const logLink = document.getElementById("loginLink");
+        logLink.innerHTML = "logout";
         }
-        logLink.innerHTML = "Login";
 }
 
 const openModal = function (e){
     e.preventDefault();
     const modal = document.querySelector("#modal1");
     modal.style.display = 'flex';
-    document.querySelector("#modalClose").addEventListener("click", closeModal);
-    modal.addEventListener("click", closeModal)
+    modal.addEventListener("click", closeModal);
+    modal.querySelector("#modalClose").addEventListener("click", closeModal);
+    modal.querySelector(".modal-content").addEventListener("click", stopPropagation);
 }
 
 const closeModal = function (e) {
-    e.preventDefault();
     const modal = document.querySelector("#modal1");
+    e.preventDefault();
     modal.style.display = 'none';
     modal.removeEventListener("click", closeModal)
-    document.querySelector("#modalClose").removeEventListener("click", closeModal);
+    modal.querySelector("#modalClose").removeEventListener("click", closeModal);
+    modal.querySelector(".modal-content").removeEventListener("click", stopPropagation);
 }
 
 const stopPropagation = function (e) {
     e.stopPropagation()
 }
+
 
